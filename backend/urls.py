4970a01a4ec11770235e073d530c6341885efd36
unknown <jonathan@ilustraviz.com>
2023-07-14 17:03:55 -0300

    last

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,7 @@ from .views import UserViewSet, GroupViewSet
 from django.contrib import admin
 from django.urls import path
 from core.views import ListViewSet, ItemViewSet
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -13,5 +14,6 @@ router.register(r'item', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token, name='api-tokn-auth'),
     path('admin/', admin.site.urls),
 ]
